56f362676a7e2ee9a97b5b3fc914aadfa27a30e3
Ali Bayramli <bayramliali@tutanota.com>
2023-04-27 21:26:34 +0400

    Add auth route guards

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,6 +1,24 @@
-import { AuthService } from '../services/auth.service';
 import { inject } from '@angular/core';
+import { Auth, authState } from '@angular/fire/auth';
+import { map } from 'rxjs';
 
-export const isLoggedInAuthGuard = () => {
-  return inject(AuthService).isLoggedIn$;
+const authStatus = () => {
+  const afAuth = inject(Auth);
+  return authState(afAuth).pipe();
+};
+
+export const isLoggedIn = () => {
+  return authStatus().pipe(map((status) => !!status));
+};
+
+export const isNotLoggedIn = () => {
+  return isLoggedIn().pipe(map((status) => !status));
+};
+
+export const isEmailVerified = () => {
+  return authStatus().pipe(map((status) => status?.emailVerified));
+};
+
+export const isNotEmailVerified = () => {
+  return isEmailVerified().pipe(map((status) => !status));
 };
